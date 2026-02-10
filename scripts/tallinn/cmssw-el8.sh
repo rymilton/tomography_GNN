8284dc77b4860ee5d98c6ae82e6b8ccdb62151e4
rymilton <rmilton@ucla.edu>
2026-02-10 15:10:03 -0800

    Removed unused script files

--- scripts/tallinn/cmssw-el8.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p main
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=3G
-#SBATCH -o logs/slurm-%x-%j-%N.out
-
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-export UNPACKED_IMAGE=/cvmfs/singularity.opensciencegrid.org/cmssw/cms\:rhel8-x86_64/
-cmssw-el8 -B /root -B /cms -B /local -B /scratch/persistent -B /scratch/local --command-to-run $@
